2f1d4d185e5c0dba66cd0fe316f4be7592f2891e
Piyush Kumar Agarwal <chatwithapiyush@gmail.com>
2025-04-28 18:43:59 +0530

    Fix CORS configuration

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 const app = require('../dist/app').default;
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+    // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', 'https://break-your-boredom.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
+        // Handle preflight request
         return res.status(200).end();
     }
 
+    // Forward other requests to Express app
     return app(req, res);
 };
